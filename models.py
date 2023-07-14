0ef0c7abcda619591de95f329969c8f2c3e498be
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-07-14 21:31:24 +0300

    connect & models

---
+++ models.py
@@ -0,0 +1,16 @@
+from mongoengine import ListField, Document, StringField, ReferenceField
+
+
+class Authors(Document):
+    fullname = StringField(max_length=30, required=True)
+    born_date = StringField(max_length=20)
+    born_location = StringField(max_length=30)
+    description = StringField(required=True)
+
+
+class Quotes(Document):
+    tags = ListField(StringField(max_length=20))
+    author = ReferenceField(Authors)
+    quote = StringField(required=True)
+
+
