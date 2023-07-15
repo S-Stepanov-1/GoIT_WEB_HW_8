ce17651138110f9a41a6e2997c9a533bd0ad0d7d
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-07-15 19:58:56 +0300

    fixed caching

--- queries.py
+++ queries.py
@@ -8,34 +8,42 @@ from models import Authors, Quotes
 
 #  connect to Redis local server
 client = redis.StrictRedis(host="localhost", port=6379, password=None)
-cache = RedisLRU(client)
+cache = RedisLRU(client, default_ttl=60 * 20)
 
 
 @cache
 def do_name_query(query_author):
+    result = []
     author = Authors.objects.get(fullname__istartswith=query_author)
 
     quotes = Quotes.objects(author=author)
     if quotes:
         print(f"Quotes by {author.fullname}:")
         for quote in quotes:
-            print(quote.quote)
-        print()
+            result.append(quote.quote)
+        return result
     else:
         print("There are no quotes by this author.")
 
 
 @cache
 def do_tag_query(query_tag):
+    result = []
     query_tag_list = query_tag.split(",")
 
-    matching_quotes = Quotes.objects(tags__in=query_tag_list)
+    matching_quotes = Quotes.objects(tags__in=query_tag_list)  # Select from the database only those quotes which have the requested tags
     if matching_quotes:
 
         for quote in matching_quotes:
-            print(f"Quote by {quote.author.fullname}\nTag(s):", end=" ")
-            [print(tag, end=" ") for tag in quote.tags if tag in query_tag_list]
-            print(f"\n{quote.quote}\n")
+            one_quote = []
+
+            one_quote.append(quote.author.fullname)  # add author's fullname
+            one_quote.append([tag for tag in quote.tags if tag in query_tag_list])  # add tags
+            one_quote.append(quote.quote)  # add quote
+
+            result.append(one_quote)
+
+        return result
 
 
 def main():
@@ -46,10 +54,19 @@ def main():
         match query[0]:
             case "exit":
                 sys.exit()
+
             case "name":
-                do_name_query(query[1])
+                quotes = do_name_query(query[1])
+                [print(quote) for quote in quotes]
+                print()
+
             case "tag" | "tags":
-                do_tag_query("".join(query[1]))  # query is str again
+                result = do_tag_query("".join(query[1]))  # query is str again
+                for quote in result:
+                    print(f"Quote by {quote[0]}\nTag(s):", end=" ")
+                    [print(tag, end=" ") for tag in quote[1]]
+                    print(f"\n{quote[2]}\n")
+
             case _:
                 print(f"\n[{query[0]}] Unknown command. Please, try again.\n")
 
